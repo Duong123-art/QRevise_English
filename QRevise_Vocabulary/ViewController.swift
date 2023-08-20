2c1f4ddeef34770ffeb5d4b9719d20242602948a
Quang Duong <quang.duong@team.enouvo.com>
2023-08-20 17:55:29 +0700

    Initial Commit

---
+++ QRevise_Vocabulary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRevise_Vocabulary
+//
+//  Created by quang.duong on 20/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ QRevise_VocabularyUITests/QRevise_VocabularyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  QRevise_VocabularyUITestsLaunchTests.swift
+//  QRevise_VocabularyUITests
+//
+//  Created by quang.duong on 20/08/2023.
+//
+
+import XCTest
+
+final class QRevise_VocabularyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
